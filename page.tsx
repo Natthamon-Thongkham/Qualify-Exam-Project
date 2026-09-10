7314108d93d9010d4d2bc2bd490a8a9ece7df1e4
Natthamon-Thongkham <natthamon.thk@gmail.com>
2026-09-10 17:39:31 +0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,11 +1,5 @@
-export default function ManagePage() {
-  return (
-    <section className="simplePage">
-      <h2>Manage the exam</h2>
+import { redirect } from "next/navigation";
 
-      <p>
-        หน้านี้จะทำตาม Figma ของ Manage the exam เมื่อมีแบบหน้านั้น
-      </p>
-    </section>
-  );
+export default function Home() {
+  redirect("/admin/grading");
 }
